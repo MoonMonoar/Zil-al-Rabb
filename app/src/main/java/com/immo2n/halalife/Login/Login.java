258f71bdff0108c05864155c9387aa85fac089bb
Monoarul Islam Moon <64534598+MoonMonoar@users.noreply.github.com>
2023-12-25 20:49:29 +0600

    Login & registration - Design done

--- app/src/main/java/com/immo2n/halalife/Login/Login.java
+++ app/src/main/java/com/immo2n/halalife/Login/Login.java
@@ -3,6 +3,7 @@ package com.immo2n.halalife.Login;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,11 +17,10 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.login_page_status_bar));
 
 
-
+        binding.signup.setOnClickListener(view -> startActivity(new Intent(Login.this, Registration.class)));
     }
 }
--- app/src/main/java/com/immo2n/halalife/Login/Registration.java
+++ app/src/main/java/com/immo2n/halalife/Login/Registration.java
@@ -1,16 +1,31 @@
 package com.immo2n.halalife.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import com.immo2n.halalife.R;
+import com.immo2n.halalife.databinding.ActivityRegistrationBinding;
 
 public class Registration extends AppCompatActivity {
-
+    private ActivityRegistrationBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        binding = ActivityRegistrationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.signup_page_status_bar));
+
+
+        //Page components
+        String[] options = getResources().getStringArray(R.array.genders);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, options);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.gender.setAdapter(adapter);
+        binding.login.setOnClickListener(v-> finish());
     }
 }
